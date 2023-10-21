b5410ded4bd1aa4fcff891944036b8c69874e443
Rares Burdulea <raresburdulea7@gmail.com>
2023-10-21 18:51:13 +0100

    Created mobile version of navigation bar

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 //start of scripting zone
+
+    //VARIABLES
+    let avatarCurrent = 2
+
     //function to change the avatar icon
-        let avatarCurrent = 2
         function changeAvatar() {
             let avatarNumber = 9;
                 if(avatarCurrent < avatarNumber){
@@ -18,4 +21,17 @@
             let link = "http://www.linkedin.com/in/raresburdulea";
                 window.open(link,'_blank');
         }
+
+        //functions to hide/unhide navigation in mobile form
+        function navBarToggle() {
+            let navStatus = document.getElementById("mobileNavigation").style.display
+
+            if(navStatus == "flex") {
+                document.getElementById("mobileNavigation").style.display = "none"
+            }
+            else {
+                document.getElementById("mobileNavigation").style.display = "flex"
+            }
+        }
+
 //end of scripting zone
